7e8cea1bd2282b6e655232488c3d77892821038d
shubham.kr08092000 <shubham.kr08092000@gmail.com>
2021-12-21 00:08:16 +0530

    now data come from data base

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,19 +7,17 @@ import {
   ListItem,
   Typography,
 } from '@material-ui/core';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
 import React from 'react';
 import NextLink from 'next/link';
 import Layout from '../../components/Layout';
-import data from '../../utils/data';
 import useStyles from '../../utils/styles';
+import Product from '../../models/Product';
+import db from '../../utils/db';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+  const { product } = props;
   const classes = useStyles();
-  const router = useRouter();
-  const { slug } = router.query;
-  const product = data.products.find((a) => a.slug === slug);
   if (!product) {
     return <div>Product Not Found</div>;
   }
@@ -102,4 +100,17 @@ export default function ProductScreen() {
       </Grid>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
 }
