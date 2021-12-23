bd822012d67688b9a57babbe4779c03b5581d62a
shubham.kr08092000 <shubham.kr08092000@gmail.com>
2021-12-23 20:53:26 +0530

    made a sample user in database

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Shubham',
+      email: 'shubham@shubham.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'shreesty',
+      email: 'shreesty@shreesty.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: 'Free Shirts',
